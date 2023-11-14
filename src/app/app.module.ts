e6b5c209bb377ccb53180911720fb325bb945c7f
Daniel <daniel.p.henricks@vanderbilt.edu>
2023-11-14 16:34:54 -0600

    Changed structure a bit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // This is needed when using angular forms.
 import { AppComponent } from './app.component';
-import { FormComponent } from './form/form.component';
 import { BrowserModule } from '@angular/platform-browser'; // Needed when using ngModel.
 @NgModule({
   declarations: [
-    AppComponent,
-    FormComponent
+    AppComponent
   ],
   imports: [
     FormsModule,
